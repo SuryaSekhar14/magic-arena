67d29305a7c635263421bba527f06cd3752acd1d
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2024-07-27 00:39:47 +0530

    added player class

---
+++ modules/Player.js
@@ -0,0 +1,9 @@
+export class Player {
+    constructor(id, name, health, attack, defense) {
+        this.id = id;
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+    }
+}
