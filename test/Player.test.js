1072443018a8b4e944879a1a044bbdce6625a588
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2024-07-28 01:07:48 +0530

    Player tests added

---
+++ test/Player.test.js
@@ -0,0 +1,23 @@
+import { Player } from '../modules/Player.js';
+
+describe('Player', () => {
+    let player;
+
+    beforeEach(() => {
+        player = new Player(1, 'John', 100, 20, 50);
+    });
+
+    it('should return true when player is alive', () => {
+        expect(player.isAlive()).toBe(true);
+    });
+
+    it('should return false when player is dead', () => {
+        player.getDamaged(200);
+        expect(player.isAlive()).toBe(false);
+    });
+
+    it('should reduce player health when getting damaged', () => {
+        player.getDamaged(30);
+        expect(player.health).toBe(70);
+    });
+});
